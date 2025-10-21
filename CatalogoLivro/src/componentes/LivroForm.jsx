34ae1d84c1af4a87dd6c5ad1e4b3575b56bb06ad
GUSTAVO HENRIQUE DA SILVA RODRIGUES <gustavo.hsrodrigues2@senacsp.edu.br>
2025-10-20 22:19:54 -0300

    20/10/25

--- CatalogoLivro/src/componentes/LivroForm.jsx
+++ CatalogoLivro/src/componentes/LivroForm.jsx
@@ -1,5 +1,96 @@
-export default function LivroForm(){
-    return(
-        <h1>Dados do Livro</h1>
-    )
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import api from "../services/api";
+
+export default function LivroForm() {
+    const [titulo, setTitulo] = useState("");
+    const [paginas, setPaginas] = useState("");
+    const [categoria, setCategoria] = useState("Romance");
+    const [descricao, setDescricao] = useState("");
+
+    // Recupera o id da URL
+    const { id } = useParams();
+    // Hook para navegação programática
+    const navigate = useNavigate();
+
+    // salvar os dados
+    const salvar = async (e) => {
+        //desativa o comportamento padrão do navegador
+        e.preventDefault()
+
+        // cria o objeto com os dados do formuário
+        const dados = { titulo, paginas, categoria, descricao }
+
+        //envia o objeto para o back end pelo método post (criar)
+        await api.post("/livros", dados)
+
+        //redireciona o usuário para a rota principal (listagem de livros)
+        navigate("/")
+    }
+
+    return (
+        <div className="container card p-0 mt-5" style={{ maxWidth: "500px" }}>
+            <div className="card-header">
+                <h5>{id ? "Editar Livro" : "Novo Livro"}</h5>
+            </div>
+            <div className="card-body">
+
+                <form>
+                    <div className="mb-3">
+                        <label className="form-label">Título</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={titulo}
+                            onChange={(e) => setTitulo(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    {/* Preço e Categoria lado a lado */}
+                    <div className="row mb-3">
+                        <div className="col-md-6">
+                            <label className="form-label">N de páginas</label>
+                            <input
+                                type="number"
+                                className="form-control"
+                                value={paginas}
+                                onChange={(e) => setPaginas(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="col-md-6">
+                            <label className="form-label">Categoria</label>
+                            <select
+                                className="form-select"
+                                value={categoria}
+                                onChange={(e) => setCategoria(e.target.value)}
+                            >
+                                <option value="Romance">Romance</option>
+                                <option value="Biografia">Biografia</option>
+                                <option value="Literatura">Literatura</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Descrição</label>
+                        <textarea
+                            className="form-control"
+                            rows="3"
+                            value={descricao}
+                            onChange={(e) => setDescricao(e.target.value)}
+                            required
+                        ></textarea>
+                    </div>
+
+                    <button type="submit"  onClick={salvar} className="btn btn-success">Salvar</button>
+                    <Link className="btn btn-warning ms-2" to={`/`}>
+                        Voltar
+                    </Link>
+                </form>
+            </div>
+        </div>
+    );
 }
--- CatalogoLivro/src/componentes/LivroList.jsx
+++ CatalogoLivro/src/componentes/LivroList.jsx
@@ -1,5 +1,23 @@
-export default function LivroList(){
-    return(
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import api from "../services/api"
+
+export default function LivroList() {
+    //armazenar a lista de livros
+    const [livros, setLivros] = useState([])
+
+    //sera executado quando a pagina for carregada
+    useEffect(() => {
+        carregarLivros()
+    }, [])
+
+    //função para buscar a lista de livros do backend
+    const carregarLivros = async () => {
+        const resposta = await api.get("/livros")
+        setLivros(resposta.data)
+    }
+
+    return (
         <div className="container card p-0 mt-5">
             <div className="card-header text-center">
                 <h4>Livros no catálogo</h4>
@@ -8,7 +26,7 @@ export default function LivroList(){
             <div className="card-body">
                 <table className="table">
                     <thead>
-                        <tr> 
+                        <tr>
                             <th>ID</th>
                             <th>Título</th>
                             <th>Páginas</th>
@@ -16,17 +34,32 @@ export default function LivroList(){
                             <th>Descrição</th>
                         </tr>
                     </thead>
+
                     <tbody>
-                    <tr>
-                        <td>01</td>
-                        <td>Teste</td>
-                        <td>100</td>
-                        <td>Literatura</td>
-                        <td>Descrição do livro</td>
-                    </tr>
+                        {livros.map((livro) => (
+                            <tr Key= {livro.id}>
+                                <td>{livro.id}</td>
+                                <td>{livro.titulo}</td>
+                                <td>{livro.paginas}</td>
+                                <td>{livro.categoria}</td>
+                                <td>{livro.descricao}</td>
+                                <td>
+                                    <div className="btn-group">
+                                        <Link className="btn btn-sm btn-primary me-2" to={`/editar/${livro.id}`}>
+                                        Editar
+                                        </Link>
+                                        
+                                        <button className="btn btn-sm btn-danger">
+                                        Excluir
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+
                     </tbody>
                 </table>
-                
+
             </div>
 
         </div>
